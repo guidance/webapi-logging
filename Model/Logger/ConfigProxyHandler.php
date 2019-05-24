fd3cf30fe5e54f4f128bdb8d068454515bd3684d
Vadim Kusakin <vadim.kusakin@guidance.com>
2019-05-24 13:22:37 +0300

    Implemented configurable logging

---
+++ Model/Logger/ConfigProxyHandler.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @author      Guidance Magento Team <magento@guidance.com>
+ * @category    Guidance
+ * @package     WebapiLogging
+ * @copyright   Copyright (c) 2019 Guidance Solutions (http://www.guidance.com)
+ */
+namespace Guidance\WebapiLogging\Model\Logger;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+use Monolog\Formatter\FormatterInterface;
+use Monolog\Handler\HandlerInterface;
+
+class ConfigProxyHandler implements HandlerInterface
+{
+    protected $subject;
+    protected $configPath;
+    protected $config;
+
+    public function __construct(HandlerInterface $subject, string $configPath, ScopeConfigInterface $config)
+    {
+        $this->subject = $subject;
+        $this->configPath = $configPath;
+        $this->config = $config;
+    }
+
+    public function isHandling(array $record)
+    {
+        return $this->config->isSetFlag($this->configPath)
+            && $this->subject->isHandling($record);
+    }
+
+    public function handle(array $record)
+    {
+        return $this->subject->handle($record);
+    }
+
+    public function handleBatch(array $records)
+    {
+        return $this->subject->handleBatch($records);
+    }
+
+    public function pushProcessor($callback)
+    {
+        return $this->subject->pushProcessor($callback);
+    }
+
+    public function popProcessor()
+    {
+        return $this->subject->popProcessor();
+    }
+
+    public function setFormatter(FormatterInterface $formatter)
+    {
+        return $this->subject->setFormatter($formatter);
+    }
+
+    public function getFormatter()
+    {
+        return $this->subject->getFormatter();
+    }
+}
